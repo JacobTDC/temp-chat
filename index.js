0cddd02d3515134a4639bc367a8807e09e9e1884
JacobTDC <jacob.story.JS@gmail.com>
2017-08-17 14:23:47 -0500

    changes

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ const fs = require('fs');
 const path = require('path');
 
 const httpServer = http.Server(app);
+const io = require('socket.io')(httpServer);
 var port = process.env.PORT || 3000
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/public/index.html');
-});
+var rooms = ['1aG5ds'];
+const public = io.of('/1aG5ds');
+
+app.use(require('express').static(__dirname + '/public'));
+
 
 app.get('/connect', function(req, res){
   if (req.query.code && req.query.code.length == 6) {
@@ -19,17 +22,22 @@ app.get('/connect', function(req, res){
   }
 });
 
-app.get('/404', function(req, res){
-  res.sendFile(__dirname + '/public/404/index.html');
+app.get('/*', function(req, res, next){
+  let room = req.params[0];
+  if (room.length == 6 && rooms.includes(room)) {
+    res.sendFile(path.join(__dirname, 'room.html'));
+  } else next();
 });
 
-app.get('/*', function(req, res){
-  let loc = req.params[0];
-  if (fs.existsSync(path.join('./public', req.path))) {
-    res.sendFile(path.join(__dirname, 'public', req.path));
-  } else if (loc.length == 6) {
-    res.send(loc);
-  } else res.redirect('404');
+app.get('*', function(req, res){
+  res.status(404).sendFile(path.join(__dirname, '404.html'));
+});
+
+io.on('connection', (socket) => {
+  console.log('1');
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
 });
 
 httpServer.listen(port, function(){
